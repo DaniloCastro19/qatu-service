99657f42d9cd44f4690f945a6436ca9becca21b6
Dan_19 <Danilo.Castro@jala.university>
2025-04-09 21:32:53 -0500

    Add Dbclient configuration

--- src/config/dbClient.js
+++ src/config/dbClient.js
@@ -1,3 +1,37 @@
 import mongoose from "mongoose";
 
 import * as dotnev from 'dotenv';
+
+dotnev.config();
+
+
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+
+class DatabaseClient {
+    constructor(){
+        this.connect();
+    }
+
+    async connect(){
+        try{
+            await mongoose.connect(CONNECTION_STRING);
+            console.log("Connected to DB client");
+            
+        }catch(error){
+            console.error("Error connecting to MongoClient: ", error);
+            process.exit(1);
+        }
+    }
+
+    async closeConnection() {
+        try {
+            await mongoose.connection.close();
+            console.log('MongoClient connection closed');
+        } catch (error) {
+            console.error('Error closing MongoDB connection:', error);
+        }
+    }
+}
+
+export default new DatabaseClient();
+
