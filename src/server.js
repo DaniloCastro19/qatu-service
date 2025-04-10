f0f713c263fc056d0860d54700a95042b8be40ae
Dan_19 <Danilo.Castro@jala.university>
2025-04-09 19:31:42 -0500

    Add necessary project and usefull dev dependencies

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import app from './app.js';
-
+import displayRoutes from 'express-routemap';
 
 
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
+    displayRoutes(app);
     console.log(`Server running on port ${PORT}`);
 });
 
