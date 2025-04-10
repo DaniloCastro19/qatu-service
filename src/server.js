8550655babaae5cf91d71978108f6137b1302e64
Dan_19 <Danilo.Castro@jala.university>
2025-04-09 22:15:58 -0500

    Add services dockerization

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 const API_PREFIX = "api";
 
+import dbClient from "./config/dbClient.js"
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}/${API_PREFIX}`);
