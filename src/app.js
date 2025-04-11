6dbee980408c20d16bc4ebad3133aacd52decb8b
CrisRivas34 <Cristopher.Rivas@jala.university>
2025-04-11 19:11:18 -0400

    feature/Adding user model and user CRUD

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import expressListRoutes from 'express-list-routes';
 
+import userRoutes from './presentationLayer/routes/user.routes.js';
+
 
 import productRoutes from './presentationLayer/routes/product.routes.js'
 
@@ -12,6 +14,7 @@ import productRoutes from './presentationLayer/routes/product.routes.js'
 const app = express();
 app.use(express.json());
 app.use('/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 
 expressListRoutes(app);
--- src/dataAccessLayer/models/user.model.js
+++ src/dataAccessLayer/models/user.model.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
-const userModel = new mongoose.Schema(
-    //Define your schema here
-);
+const userModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'seller', 'customer'],
+        default: 'customer'
+      }
+    }, { timestamps: true });
 
 export default mongoose.model('user', userModel);
