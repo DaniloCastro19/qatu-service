86a7783487d348ff7a0e5f9f3873d865fcc133e8
Alejandro Valencia <alejandro.valencia@jala.university>
2025-04-19 13:54:06 -0400

    fix: Add Product CRUD and fix branch commits 2

---
+++ src/dataAccessLayer/repositories/product.repository.js
@@ -0,0 +1,29 @@
+import Product from '../models/product.model.js';
+
+export const productRepository = {
+    async getAllProducts() {
+      return Product.find();
+    },
+  
+    async getProductById(id) {
+      return Product.findById(id);
+    },
+  
+    async createProduct(data) {
+      const product = new Product(data);
+      return product.save();
+    },
+  
+    async updateProduct(id, data) {
+      return Product.findByIdAndUpdate(id, data, { new: true });
+    },
+
+    async patchProduct(id, data) {
+      const productToPatch = Product.findById(id);
+      return Product.findByIdAndUpdate(id, {...productToPatch, ...data}, { new: true });
+    },
+  
+    async deleteProduct(id) {
+      return Product.findByIdAndDelete(id);
+    }
+  };
