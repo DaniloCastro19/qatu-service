f3fb534c93c2a1b58c3cd078c77996c7f4a1ff63
Dan_19 <Danilo.Castro@jala.university>
2025-04-19 21:06:37 -0500

    Add cors configuration to fix CORS problems on Client

--- src/config/environments/environments.js
+++ src/config/environments/environments.js
@@ -5,4 +5,5 @@ export const envs = {
   PORT: env.get('PORT').required().asPortNumber(),
   CONNECTION_STRING: env.get('CONNECTION_STRING').required().asString(),
   JWT_SECRET: env.get('JWT_SECRET').required().asString(),
+  CLIENT_URL: env.get('CLIENT_URL').required().asString(),
 };
