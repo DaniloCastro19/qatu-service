303425d1baa431321c410c4f72849b29b0606dfc
Diego Terrazas Sanchez <Ttanatos@example.com>
2025-05-08 21:27:27 -0400

    Update register in user.service

--- src/businessLogicLayer/services/user.service.js
+++ src/businessLogicLayer/services/user.service.js
@@ -11,6 +11,25 @@ export const userService = {
     return await userRepository.getUserById(id);
   },
 
+  async registerService(userData) {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(userData.password, saltRounds)
+      const userToCreate = {
+        ...userData,
+        password: hashedPassword
+      };
+      return await userRepository.createUser(userToCreate);
+  },
+  
+  async getUserByEmail(email) {
+        return await userRepository.getUserByEmail(email);
+  },
+  
+  async getUserByUsername(username) {
+        return await userRepository.getUserByUsername(username);
+  },
+    
+  
   async updateUser(id, userData) {
     if (userData.password) {
       const saltRounds = 10;
