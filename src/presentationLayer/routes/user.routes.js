7d1943668306449ebd917561da900648ef118253
Diego Terrazas Sanchez <Ttanatos@example.com>
2025-05-08 21:40:35 -0400

    Update register in the user.routes

--- src/presentationLayer/routes/user.routes.js
+++ src/presentationLayer/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { userController } from '../controllers/user.controller.js';
+import { validateAuthRequest } from '../middlewares/validation.middleware.js';
 
 export const router = express.Router();
 
 router.get('/', userController.getAll);
 router.get('/:id', userController.getById);
+router.post('/register', validateAuthRequest('register'), userController.register);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
 
