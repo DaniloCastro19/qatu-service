046bce228734a2e62292f8abc2b3e085de68460a
Diego Terrazas Sanchez <Ttanatos@example.com>
2025-05-22 20:19:16 -0400

    Update middleware inactivity

--- src/config/environments/environments.js
+++ src/config/environments/environments.js
@@ -7,5 +7,5 @@ export const envs = {
   JWT_SECRET: env.get('JWT_SECRET').required().asString(),
   CLIENT_URL: env.get('CLIENT_URL').required().asString(),
   NODE_ENV:env.get('NODE_ENV').required().asString(),
-  SESSION_INACTIVITY_TIMEOUT: env.get('SESSION_INACTIVITY_TIMEOUT').default(500).asIntPositive()
+  SESSION_INACTIVITY_TIMEOUT: env.get('SESSION_INACTIVITY_TIMEOUT').asIntPositive()
 };
--- src/presentationLayer/middlewares/inactivity.middleware.js
+++ src/presentationLayer/middlewares/inactivity.middleware.js
@@ -1,39 +1,40 @@
 import jwt from 'jsonwebtoken';
-import { envs } from '../../config/environments/environments.js';
 import { userRepository } from '../../dataAccessLayer/repositories/user.repository.js';
+import { envs } from '../../config/environments/environments.js';
 import { AppError } from '../../businessLogicLayer/errors/error.js';
 
 export const inactivityMiddleware = async (req, res, next) => {
 try {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) return next();
-    
+
     const decoded = jwt.verify(token, envs.JWT_SECRET);
     const now = Math.floor(Date.now() / 1000);
+    
     const expirationTime = decoded.iat + envs.SESSION_INACTIVITY_TIMEOUT;
 
-        req.sessionInfo = {
-        remainingTime: expirationTime - now,
-        expiresAt: new Date(expirationTime * 1000)
-        };
+    req.sessionInfo = {
+        remainingTime: Math.max(0, expirationTime - now),
+      expiresAt: new Date(expirationTime * 1000)
+    };
 
-        if (expirationTime < now) {
+    if (now > expirationTime) {
         await userRepository.registerAutomaticLogout(decoded.id);
-        return next(new AppError(401, 'Your session has expired due to inactivity', {
+        return next(new AppError(401, 'Sesión cerrada por inactividad (100 segundos)', {
             code: 'SESSION_TIMEOUT',
             autoLogout: true
         }));
-        }
+    }
+
         await userRepository.updateLastActivity(decoded.id);
         next();
-    }  catch (error) {
-    console.error('[Session Error]', error.message);
-    if (error.name === 'TokenExpiredError') {
-        return next(new AppError(401, 'Session has expired', {
-        code: 'TOKEN_EXPIRED',
-        autoLogout: true
-    }));
-    }
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+        return next(new AppError(401, 'Token JWT expirado', {
+            code: 'TOKEN_EXPIRED',
+            autoLogout: true
+        }));
+        }
     next(error);
-  }
+}
 };
